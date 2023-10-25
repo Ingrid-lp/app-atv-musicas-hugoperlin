9c606e43e8fe52d7a99a63723316d0b6bd7b9e5d
ingrid <ingrid4519v@gmail.com>
2023-10-25 10:52:44 -0300

    pronto

--- src/main/java/ifpr/pgua/eic/colecaomusicas/controllers/Principal.java
+++ src/main/java/ifpr/pgua/eic/colecaomusicas/controllers/Principal.java
@@ -1,24 +1,29 @@
 package ifpr.pgua.eic.colecaomusicas.controllers;
 
 import ifpr.pgua.eic.colecaomusicas.App;
+import io.github.hugoperlin.navigatorfx.BorderPaneRegion;
 import javafx.fxml.FXML;
+import javafx.util.Callback;
 
 public class Principal {
     
 
     @FXML
     private void cadastrarGenero(){
-        App.pushScreen("CADASTROGENERO");
+        //App.pushScreen("CADASTROGENERO");
+        App.changeScreenRegion("CADASTROGENERO",BorderPaneRegion.CENTER);
     }
 
     @FXML
     private void cadastrarArtista(){
-        App.pushScreen("CADASTROARTISTA");
+        //App.pushScreen("CADASTROARTISTA");
+        App.changeScreenRegion("CADASTROARTISTA", BorderPaneRegion.CENTER);
     }
 
     @FXML
     private void listarGeneros(){
-        App.pushScreen("LISTARGENEROS");
+        //App.pushScreen("LISTARGENEROS");
+        App.changeScreenRegion("LISTARGENRO", BorderPaneRegion.CENTER);
     }
 
     @FXML
